216b7b88633f0a591c187bcb49360a37e5d9871c
Hironobu-okubo <ee171068oh@gmail.com>
2022-05-18 22:08:29 +0900

    create 050

---
+++ ABC/A/050-Addition and subtraction Easy.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  int a,b;
+  string p;
+  cin >> a >> p >> b;
+  if(p == "+") cout << a + b << endl;
+  else cout << a - b << endl;
+}
