51edd86aa64ab02383a87c711dc1801b0ca5277b
Hironobu-okubo <ee171068oh@gmail.com>
2022-05-18 22:12:14 +0900

    create 038

---
+++ ABC/A/038-お茶.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+ 
+  string s;
+  cin >> s ;
+  int n = s.size();
+  if(s[n - 1] == 'T') cout << "YES" << endl;
+  else cout << "NO" << endl;
+}
