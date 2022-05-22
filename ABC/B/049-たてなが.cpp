1cab3aa312e5923bbda7cf90c31f60887be53fc2
Hironobu-okubo <ee171068oh@gmail.com>
2022-05-22 20:37:29 +0900

    create B049

---
+++ ABC/B/049-たてなが.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int h, w;
+  string c;
+  cin >> h >> w;
+  for(int i = 0; i < h; i++){
+  	cin >> c;
+    cout << c << endl;
+    cout << c << endl;
+  }
+}
