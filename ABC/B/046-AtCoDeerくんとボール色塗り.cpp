0f4da62b1a120086cdca5e8a2227bb0960848fff
Hironobu-okubo <ee171068oh@gmail.com>
2022-05-22 19:44:16 +0900

    create B046

---
+++ ABC/B/046-AtCoDeerくんとボール色塗り.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int n,k;
+  cin >> n >> k;
+  int total = 1;
+  for(int i = 0; i < n; i++){
+  	if(i == 0) total *= k;
+    else total *= k - 1;
+  }
+  cout << total;
+  return 0;
+}
