de5fcd4794e6e7d6c5d96e40bb80547f4c086a41
Hironobu-okubo <ee171068oh@gmail.com>
2022-05-27 00:02:18 +0900

    create A059

---
+++ ABC/A/059-Three-letter acronym.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+  string a,b,c;
+  cin >> a >> b >> c;
+  string ini = "";
+  ini += toupper(a[0]);
+  ini += toupper(b[0]);
+  ini += toupper(c[0]);
+  cout << ini <<endl;
+}
