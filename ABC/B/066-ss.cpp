33000c567f4097a417613a97e858c7e5e1f3bfd3
Hironobu-okubo <ee171068oh@gmail.com>
2022-06-02 01:24:13 +0900

    create Tenka1

--- ABC/B/066-ss.cpp
+++ ABC/B/066-ss.cpp
@@ -1,17 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 int main() {
-  string s;
-  cin >> s;
-  int lenNum = s.size();
-  for(int i = 0; i < lenNum; i++){
-  	s.pop_back();
-    string s1 = s.substr(0,s.size()/2);
-    string s2 = s.substr(s.size()/2);
-    if(s1 == s2){
-      cout << s.size() << endl;
-      exit(0);
+  int n;
+  cin >> n;
+  int x = -1,y = -1;
+  int ans = 0;
+  vector<int> a(n),b(n);
+  for(int i = 0; i < n; i++){
+  	cin >> a[i] >> b[i];
+    if(x < a[i]){
+      x = a[i];
+      y = b[i];
+      ans = x + y;
     }
   }
+  cout << ans;
+  return 0;
 }
