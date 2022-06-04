2af7e305d92fba7a5bc77d0ff0ad6679bf260e0c
Hironobu-okubo <ee171068oh@gmail.com>
2022-06-05 00:14:52 +0900

    create AntBook

---
+++ AntBook/fibonacci.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+/* alias */
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+/* define short */
+#define pb push_back
+#define mp make_pair
+#define all(obj) (obj).begin(), (obj).end()
+#define YESNO(bool) if(bool){cout<<"YES"<<endl;}else{cout<<"NO"<<endl;}
+#define yesno(bool) if(bool){cout<<"yes"<<endl;}else{cout<<"no"<<endl;}
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+/* REP macro */
+#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define rep(i, n) reps(i, 0, n)
+#define rrep(i, n) reps(i, 1, n + 1)
+#define repd(i,n) for(ll i=n-1;i>=0;i--)
+#define rrepd(i,n) for(ll i=n;i>=1;i--)
+#define out(x) cout << x << endl
+
+int n = 6;
+vi memo(n + 1);
+int fib(int n){
+    if(n <= 1) return memo[n] = n;
+    if(memo[n] != 0) return memo[n];
+    return memo[n] = fib(n - 1) + fib(n - 2);
+}
+
+int main(){
+    out(fib(n));
+    rep(i,n + 1) out(memo[i]);
+
+}
