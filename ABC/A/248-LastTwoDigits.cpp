e292cd72c50a884ffd686059fcd64863a6fad77a
Hironobu-okubo <ee171068oh@gmail.com>
2022-06-04 22:04:54 +0900

    create 248A

---
+++ ABC/A/248-LastTwoDigits.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+using VI = vector<int>;
+typedef long long ll ;
+#define all(a) (a).begin(),(a).end()
+#define out(x) cout << x << endl
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#define repp(i, n) for(int i = 1; i <= (int)(n); i++)
+#define pb push_back
+
+int main(){
+    string n;
+    cin >> n;
+    cout << n[1] << n[2] <<endl;
+}
