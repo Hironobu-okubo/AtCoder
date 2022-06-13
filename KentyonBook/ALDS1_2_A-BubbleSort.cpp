179a6eea082e1f257a9165f407a755b3c6b50cdc
Hironobu-okubo <ee171068oh@gmail.com>
2022-06-14 00:56:28 +0900

    create Bubble Sort

--- KentyonBook/ALDS1_2_A-BubbleSort.cpp
+++ KentyonBook/ALDS1_2_A-BubbleSort.cpp
@@ -21,33 +21,34 @@ using pii = pair<int, int>;
 /* REP macro */
 #define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
 #define rep(i, n) reps(i, 0, n)
+#define repp(i,n) reps(i, 0, n + 1)
 #define rrep(i, n) reps(i, 1, n + 1)
 #define repd(i,n) for(ll i=n-1;i>=0;i--)
 #define rrepd(i,n) for(ll i=n;i>=1;i--)
 #define out(x) cout << x << endl
-
-bool func(int i, int w, const vi &a){
-    //ベースケース
-    if(i == 0){
-        if(w == 0) return true;
-        else return false;
-    }
-    //a[i - 1]を選ばない場合
-    if(func(i - 1, w, a)) return true;
-    //a[i - 1]を選ぶ場合
-    if(func(i - 1, w - a[i - 1], a)) return true;
-
-    return false;
-    
-}
+//定数
+#define INF32 2147483647 
+#define INF64 9223372036854775807 
+#define MOD 1000000007 
+//関数
+template <typename T> bool chmin(T& a, const T b) { if (a>b) { a=b; return 1; } return 0; }
+template <typename T> bool chmax(T& a, const T b) { if (a<b) { a=b; return 1; } return 0; }
 
 int main(){
-    int n,w;
-    cin >> n >>w;
+    int n,j;
+    cin >> n;
     vi a(n);
     rep(i,n) cin >> a[i];
-
-    //再帰的に解く
-    if(func(n, w, a)) out("Yes");
-    else out("No");
+    bool flag = true;
+    for(int i = 0; flag; i++){
+        flag = false;
+        for(int j = n - 1; j >= i; j--){
+            if(a[j] < a[j - 1]){
+                swap(a[j],a[j - 1]);
+                flag = true;
+            }
+        }
+    }
+    rep(i,n) out(a[i]);
+    
 }
