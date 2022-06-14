75bd7f99385674fb4222b6d11364f4b909652d5e
Hironobu-okubo <ee171068oh@gmail.com>
2022-06-15 00:36:51 +0900

    retry 045B

--- ABC/B/045-3人でカードゲームイージー.cpp
+++ ABC/B/045-3人でカードゲームイージー.cpp
@@ -1,5 +1,75 @@
 #include <bits/stdc++.h>
 using namespace std;
+/* alias */
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+/* define short */
+#define pb push_back
+#define mp make_pair
+#define all(obj) (obj).begin(), (obj).end()
+#define YESNO(bool) if(bool){cout<<"YES"<<endl;}else{cout<<"NO"<<endl;}
+#define yesno(bool) if(bool){cout<<"yes"<<endl;}else{cout<<"no"<<endl;}
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+/* REP macro */
+#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define rep(i, n) reps(i, 0, n)
+#define repp(i,n) reps(i, 0, n + 1)
+#define rrep(i, n) reps(i, 1, n + 1)
+#define repd(i,n) for(ll i=n-1;i>=0;i--)
+#define rrepd(i,n) for(ll i=n;i>=1;i--)
+#define out(x) cout << x << endl
+//定数
+#define INF32 2147483647 
+#define INF64 9223372036854775807 
+#define MOD 1000000007 
+
+
+int main(){
+    string a, b, c;
+    cin >> a >> b >> c;
+    char nxt = 'a';
+    bool ifZero = true;
+    int num_a = 0,num_b = 0,num_c = 0;
+    while(ifZero){
+      if(nxt == 'a'){
+        nxt = a[num_a];
+        num_a++;
+      }
+      else if(nxt == 'b'){
+        nxt = b[num_b];
+        num_b++;
+      }
+      else if(nxt == 'c'){
+        nxt = c[num_c];
+        num_c++;
+      }
+      if(num_a > a.size()){
+        out("A");
+        ifZero = false;
+      }
+      if(num_b > b.size()){
+        out("B");
+        ifZero = false;
+      }
+      if(num_c > c.size()){
+        out("C");
+        ifZero = false;
+      }
+
+    }
+
+}
+/*
+#include <bits/stdc++.h>
+using namespace std;
 
 int main() {
   string a,b,c;
@@ -43,3 +113,4 @@ int main() {
   }
   return 0;
 }
+*/
