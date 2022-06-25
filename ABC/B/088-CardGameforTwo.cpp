f813792a3d7d71afda0451cbe9e3c34ee0f3d554
Hironobu-okubo <ee171068oh@gmail.com>
2022-06-25 19:13:42 +0900

    retry 88B

--- ABC/B/088-CardGameforTwo.cpp
+++ ABC/B/088-CardGameforTwo.cpp
@@ -21,11 +21,33 @@ using pii = pair<int, int>;
 /* REP macro */
 #define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
 #define rep(i, n) reps(i, 0, n)
+#define repp(i,n) reps(i, 0, n + 1)
 #define rrep(i, n) reps(i, 1, n + 1)
 #define repd(i,n) for(ll i=n-1;i>=0;i--)
 #define rrepd(i,n) for(ll i=n;i>=1;i--)
 #define out(x) cout << x << endl
+//定数
+#define INF32 2147483647 
+#define INF64 9223372036854775807 
+#define MOD 1000000007 
 
+int main(){
+    int n;
+    cin >> n;
+    vi a(n);
+    int alice = 0, bob = 0;
+    rep(i,n){
+        cin >> a[i];
+    }
+    sort(all(a),greater<int>());
+    rep(i,n){
+        if(i % 2 == 0) alice += a[i];
+        else bob += a[i];
+    }
+    out(alice - bob);
+}
+
+/*
 int main(){
     int n,alice = 0,Bob = 0;
     cin >> n;
@@ -40,3 +62,4 @@ int main(){
     }
     out(alice - Bob);
 }
+*/
