1fe9b9af606aade9dba7f9ed89fbe00a31a9a838
Hironobu-okubo <ee171068oh@gmail.com>
2022-06-26 00:02:02 +0900

    retry 91B

--- ABC/B/091-TwoColorsCardsGame.cpp
+++ ABC/B/091-TwoColorsCardsGame.cpp
@@ -21,11 +21,43 @@ using pii = pair<int, int>;
 /* REP macro */
 #define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
 #define rep(i, n) reps(i, 0, n)
+#define repp(i,n) reps(i, 0, n + 1)
 #define rrep(i, n) reps(i, 1, n + 1)
 #define repd(i,n) for(ll i=n-1;i>=0;i--)
 #define rrepd(i,n) for(ll i=n;i>=1;i--)
 #define out(x) cout << x << endl
+//定数
+#define INF32 2147483647 
+#define INF64 9223372036854775807 
+#define MOD 1000000007 
 
+int main(){
+    int n, m;
+    cin >> n;
+    vs s(n),t(m);
+    rep(i,n) cin >> s[i];
+    cin >> m;
+    rep(i,m) cin >> t[i];
+    int ans = 0;
+    rep(i,n){
+        int count = 0;
+        rep(j,n){
+            if(s[i] == s[j]) count++;
+        }
+        rep(k,m){
+            if(s[i] == t[k]) count--;
+        }
+        ans = max(ans,count);
+    }
+    out(ans);
+}
+
+
+
+
+
+
+/*
 int main(){
     int n,m,ans = 0;
     vs s(101);
@@ -48,3 +80,4 @@ int main(){
     }
     out(ans);
 }
+*/
