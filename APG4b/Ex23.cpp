2b49d05cdebce54e0cfad0c03ce9461c2f292cae
Hironobu-okubo <ee171068oh@gmail.com>
2022-10-15 04:56:28 +0900

    retry Ex23

--- APG4b/Ex23.cpp
+++ APG4b/Ex23.cpp
@@ -1,3 +1,52 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// /* alias */
+// using ull = unsigned long long;
+// using ll = long long;
+// using vi = vector<int>;
+// using vl = vector<long>;
+// using vll = vector<long long>;
+// using vvi = vector<vi>;
+// using vvl = vector<vl>;
+// using vvll = vector<vll>;
+// using vs = vector<string>;
+// using pii = pair<int, int>;
+// /* define short */
+// #define pb push_back
+// #define mp make_pair
+// #define all(obj) (obj).begin(), (obj).end()
+// #define YESNO(bool) if(bool){cout<<"YES"<<endl;}else{cout<<"NO"<<endl;}
+// #define yesno(bool) if(bool){cout<<"yes"<<endl;}else{cout<<"no"<<endl;}
+// #define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+// /* REP macro */
+// #define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+// #define rep(i, n) reps(i, 0, n)
+// #define rrep(i, n) reps(i, 1, n + 1)
+// #define repd(i,n) for(ll i=n-1;i>=0;i--)
+// #define rrepd(i,n) for(ll i=n;i>=1;i--)
+// #define out(x) cout << x << endl
+
+// int main(){
+//     int n;
+//     cin >> n;
+//     vi a(n);
+//     rep(i,n) cin >> a[i];
+//     map<int, int> cnt;
+//     for(int x : a){
+//         if(cnt.count(x)) cnt.at(x)++;
+//         else cnt[x] = 1;
+//     }
+//     int max_cnt = 0;
+//     int ans = -1;
+//     for(int x : a){
+//         if(max_cnt < cnt.at(x)){
+//             max_cnt = cnt.at(x);
+//             ans = x;
+//         }
+//     }
+//     cout << ans << " " << max_cnt << endl;
+// }
+
 #include <bits/stdc++.h>
 using namespace std;
 /* alias */
@@ -21,28 +70,39 @@ using pii = pair<int, int>;
 /* REP macro */
 #define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
 #define rep(i, n) reps(i, 0, n)
+#define repp(i,n) reps(i, 0, n + 1)
 #define rrep(i, n) reps(i, 1, n + 1)
 #define repd(i,n) for(ll i=n-1;i>=0;i--)
 #define rrepd(i,n) for(ll i=n;i>=1;i--)
 #define out(x) cout << x << endl
+//定数
+#define INF32 2147483647 
+#define INF64 9223372036854775807 
+#define MOD 1000000007
 
 int main(){
-    int n;
-    cin >> n;
-    vi a(n);
-    rep(i,n) cin >> a[i];
-    map<int, int> cnt;
-    for(int x : a){
-        if(cnt.count(x)) cnt.at(x)++;
-        else cnt[x] = 1;
+  int N;
+  cin >> N;
+  vi A(N);
+  rep(i,N){
+    cin >> A[i];
+  }
+  map<int,int> mode;
+  for(auto i : A){
+    if(mode[i] != 0 ){
+      mode[i] += 1;
     }
-    int max_cnt = 0;
-    int ans = -1;
-    for(int x : a){
-        if(max_cnt < cnt.at(x)){
-            max_cnt = cnt.at(x);
-            ans = x;
-        }
+    else{
+      mode[i] = 1;
     }
-    cout << ans << " " << max_cnt << endl;
+  }
+  int max_num = 0;
+  int max_mode = -1;
+  for(auto i : A){
+    if(max_num < mode[i]){
+      max_num = mode[i];
+      max_mode = i;
+    }
+  }
+  cout << max_mode << ' ' << max_num << endl;
 }
---
+++ APG4b/Ex23_practice.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+/* alias */
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using pii = pair<int, int>;
+/* define short */
+#define pb push_back
+#define mp make_pair
+#define all(obj) (obj).begin(), (obj).end()
+#define YESNO(bool) if(bool){cout<<"YES"<<endl;}else{cout<<"NO"<<endl;}
+#define yesno(bool) if(bool){cout<<"yes"<<endl;}else{cout<<"no"<<endl;}
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+/* REP macro */
+#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define rep(i, n) reps(i, 0, n)
+#define repp(i,n) reps(i, 0, n + 1)
+#define rrep(i, n) reps(i, 1, n + 1)
+#define repd(i,n) for(ll i=n-1;i>=0;i--)
+#define rrepd(i,n) for(ll i=n;i>=1;i--)
+#define out(x) cout << x << endl
+//定数
+#define INF32 2147483647 
+#define INF64 9223372036854775807 
+#define MOD 1000000007
+
+int main(){
+  // map<int,int> dic;
+  // dic[0] = 0;
+  // dic[1] = 1;
+  // dic.erase(0);
+  // cout << dic.at(1) << endl;
+  // cout << dic.count(1) << endl;
+  // dic[2] = 2;
+  // cout << dic.size() << endl;
+  // cout << dic.count(0) << endl;
+
+  queue<int> que;
+  que.push(0);
+  que.push(1);
+  que.push(2);
+  cout << que.front() << endl;
+  out(que.size());
+  que.pop();
+  out(que.size());
+
+}
