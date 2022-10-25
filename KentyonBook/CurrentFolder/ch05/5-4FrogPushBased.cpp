d516d9b34702e8b6f5ee087fc1b8b51706353fad
Hironobu-okubo <ee171068oh@gmail.com>
2022-10-25 18:02:47 +0900

    FrogRecursion

---
+++ KentyonBook/CurrentFolder/ch05/5-4FrogPushBased.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+/* alias */
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using vc = vector<char>;
+using pii = pair<int, int>;
+/* define short */
+#define pb push_back
+#define mp make_pair
+#define all(obj) (obj).begin(), (obj).end()
+#define YESNO(bool) if(bool){cout<<"YES"<<endl;}else{cout<<"NO"<<endl;}
+#define yesno(bool) if(bool){cout<<"yes"<<endl;}else{cout<<"no"<<endl;}
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+/* REP macro */
+#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define rep(i, n) reps(i, 0, n)
+#define repp(i,n) reps(i, 0, n + 1)
+#define rrep(i, n) reps(i, 1, n + 1)
+#define repd(i,n) for(ll i=n-1;i>=0;i--)
+#define rrepd(i,n) for(ll i=n;i>=1;i--)
+#define out(x) cout << x << endl
+//定数
+#define INF32 2147483647 
+#define INF64 9223372036854775807 
+#define MOD 1000000007
+
+template<class T> void chmin(T& a,T b){
+  if(a > b){
+    a = b;
+  }
+}
+
+int main(){
+  int N;
+  cin >> N;
+  vi h(N);
+  rep(i,N) cin >> h[i];
+  vi dp(N,INF32);
+  
+  dp[0] = 0;
+  rep(i,N){
+    if(i + 1 < N){
+      chmin(dp[i + 1],dp[i] + abs(h[i] - h[i + 1]));
+    }
+    if(i + 2 < N){
+      chmin(dp[i + 2],dp[i] + abs(h[i] - h[i + 2]));
+    }
+  }
+  out(dp[N - 1]);
+}
