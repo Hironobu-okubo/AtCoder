7cadb862ad895b9b133b275b4aba1bcfda4b2219
Hironobu-okubo <ee171068oh@gmail.com>
2022-11-08 14:31:24 +0900

    Knapsack

--- MathAlgorithm/tempCodeRunnerFile.cpp
+++ MathAlgorithm/tempCodeRunnerFile.cpp
@@ -1,2 +1,9 @@
-a.assign(0,n);
-  rep(i,n) cin >> a[i];
+rep(i,n){
+  //   rep(j,w + 1){
+  //     if(w - weight[j] < 0)  dp[i][j] = dp[i - 1][j] ;
+  //     else{
+  //       dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - weight[i]] + value[i]);
+  //     }
+  //   }
+  //   out(dp[n][w]);
+  // }
