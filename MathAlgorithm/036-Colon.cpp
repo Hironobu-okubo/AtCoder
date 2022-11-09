72aa44784eea7a1cf123c73bf5aa43462eea804a
Hironobu-okubo <ee171068oh@gmail.com>
2022-11-09 12:20:08 +0900

    ClockCoordinate

---
+++ MathAlgorithm/036-Colon.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+/* alias */
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using vc = vector<char>;
+using pii = pair<int, int>;
+/* define short */
+#define pb push_back
+#define mp make_pair
+#define all(obj) (obj).begin(), (obj).end()
+#define YESNO(bool) if(bool){cout<<"YES"<<endl;}else{cout<<"NO"<<endl;}
+#define yesno(bool) if(bool){cout<<"yes"<<endl;}else{cout<<"no"<<endl;}
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+/* REP macro */
+#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define rep(i, n) reps(i, 0, n)
+#define repp(i,n) reps(i, 0, n + 1)
+#define rrep(i, n) reps(i, 1, n + 1)
+#define repd(i,n) for(ll i=n-1;i>=0;i--)
+#define rrepd(i,n) for(ll i=n;i>=1;i--)
+#define out(x) cout << x << endl
+//定数
+#define INF32 2147483647 
+#define INF64 9223372036854775807 
+#define MOD 1000000007
+
+template<typename T> inline bool chmax(T &a, T b) { return ((a < b) ? (a = b, true) : (false)); }
+template<typename T> inline bool chmin(T &a, T b) { return ((a > b) ? (a = b, true) : (false)); }
+
+int main(){
+  int a, b, h, m;
+  cin >> a >> b >> h >> m;
+  
+  double angleH = 30 * h + 0.5 * m;
+  double angleM = 6 * m;
+  double Hx = a * cos(angleH * M_PI / 180);
+  double Hy = a * sin(angleH * M_PI / 180);
+  double Mx = b * cos(angleM * M_PI / 180);
+  double My = b * sin(angleM * M_PI / 180);
+  // cout << angleH << " " << angleM << " " << Hx << " " << Hy << " " << Mx << " " << My << " "; 
+  double ans = sqrt(pow((Hx - Mx),2.0) + pow((Hy - My),2.0));
+  printf("%.12lf",ans);
+}
