286ee33b21b7fe1e0e0d6bebed8d2da401fafad2
Hironobu-okubo <ee171068oh@gmail.com>
2022-11-09 17:06:50 +0900

    NewtonMethod

---
+++ MathAlgorithm/NewtonMethod.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+/* alias */
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using vc = vector<char>;
+using pii = pair<int, int>;
+/* define short */
+#define pb push_back
+#define mp make_pair
+#define all(obj) (obj).begin(), (obj).end()
+#define YESNO(bool) if(bool){cout<<"YES"<<endl;}else{cout<<"NO"<<endl;}
+#define yesno(bool) if(bool){cout<<"yes"<<endl;}else{cout<<"no"<<endl;}
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+/* REP macro */
+#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define rep(i, n) reps(i, 0, n)
+#define repp(i,n) reps(i, 0, n + 1)
+#define rrep(i, n) reps(i, 1, n + 1)
+#define repd(i,n) for(ll i=n-1;i>=0;i--)
+#define rrepd(i,n) for(ll i=n;i>=1;i--)
+#define out(x) cout << x << endl
+//定数
+#define INF32 2147483647 
+#define INF64 9223372036854775807 
+#define MOD 1000000007
+
+template<typename T> inline bool chmax(T &a, T b) { return ((a < b) ? (a = b, true) : (false)); }
+template<typename T> inline bool chmin(T &a, T b) { return ((a > b) ? (a = b, true) : (false)); }
+
+int main(){
+  // 求めたい値
+  double a = 2;
+  // 初期値
+  double initA = 2;
+
+  reps(i,1,6){
+    // xとyの座標を求める
+    double x = initA;
+    double y = initA * initA;
+
+    // 接戦の式を求める
+    double dif = 2.0 * x;
+    double difA = y - dif * x;
+
+    // 次のaの値を求める
+    double nextA = (a - difA) / dif;
+    printf("Step #%d: a = %.12lf -> %.12lf\n",i,initA,nextA);
+    initA = nextA;
+  }
+}
