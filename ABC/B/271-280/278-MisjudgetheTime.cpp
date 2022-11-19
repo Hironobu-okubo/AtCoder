bc58873546e915aed3bd33a5924efe6998f073f3
Hironobu-okubo <ee171068oh@gmail.com>
2022-11-19 23:36:08 +0900

    278

---
+++ ABC/B/271-280/278-MisjudgetheTime.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+/* alias */
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using vc = vector<char>;
+using pii = pair<int, int>;
+/* define short */
+#define pb push_back
+#define mp make_pair
+#define all(obj) (obj).begin(), (obj).end()
+#define YESNO(bool) if(bool){cout<<"YES"<<endl;}else{cout<<"NO"<<endl;}
+#define yesno(bool) if(bool){cout<<"yes"<<endl;}else{cout<<"no"<<endl;}
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+/* REP macro */
+#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define rep(i, n) reps(i, 0, n)
+#define repp(i,n) reps(i, 0, n + 1)
+#define rrep(i, n) reps(i, 1, n + 1)
+#define repd(i,n) for(ll i=n-1;i>=0;i--)
+#define rrepd(i,n) for(ll i=n;i>=1;i--)
+#define out(x) cout << x << endl
+//定数
+#define INF32 2147483647 
+#define INF64 9223372036854775807 
+#define MOD 1000000007
+
+template<typename T> inline bool chmax(T &a, T b) { return ((a < b) ? (a = b, true) : (false)); }
+template<typename T> inline bool chmin(T &a, T b) { return ((a > b) ? (a = b, true) : (false)); }
+
+int main(){
+  int h,m;
+  cin >> h >> m;
+  while(true){
+    while(true){
+      int tmp1 = (m / 10), tmp2 = m % 10;
+      int tmp3 = h / 10, tmp4 = h % 10;
+      int tmph = tmp3 * 10 + tmp1;
+      int tmpm = tmp4 * 10 + tmp2;
+      if(tmpm >= 0 && tmpm < 60 && tmph >= 0 && tmph < 24){
+        cout << h << " " << m << endl;
+        return 0;
+      }
+      if(m == 59) break;
+      m++;
+    }
+    if(h == 23) h = 0;
+    else h++;
+    m = 0;
+  }
+  
+  cout << h << " " << m;
+}
