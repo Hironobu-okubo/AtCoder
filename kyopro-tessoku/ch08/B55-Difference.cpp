2c20c25f5f9778e7ccb9665d54e7006602ee173e
Hironobu-okubo <ee171068oh@gmail.com>
2022-11-30 10:32:51 +0900

    B55

---
+++ kyopro-tessoku/ch08/B55-Difference.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+/* alias */
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+using pii = pair<int, int>;
+/* define short */
+#define pb push_back
+#define mp make_pair
+#define all(obj) (obj).begin(), (obj).end()
+#define YESNO(bool) if(bool){cout<<"YES"<<endl;}else{cout<<"NO"<<endl;}
+#define yesno(bool) if(bool){cout<<"yes"<<endl;}else{cout<<"no"<<endl;}
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+/* REP macro */
+#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define rep(i, n) reps(i, 0, n)
+#define repp(i,n) reps(i, 0, n + 1)1
+#define rrep(i, n) reps(i, 1, n + 1)
+#define repd(i,n) for(ll i=n-1;i>=0;i--)
+#define rrepd(i,n) for(ll i=n;i>=1;i--)
+#define out(x) cout << x << endl
+//定数
+#define INF32 2147483647 
+#define INF64 9223372036854775807 
+#define MOD 1000000007
+
+template<typename T> inline bool chmax(T &a, T b) { return ((a < b) ? (a = b, true) : (false)); }
+template<typename T> inline bool chmin(T &a, T b) { return ((a > b) ? (a = b, true) : (false)); }
+
+int main(){
+  int q;
+  cin >> q;
+  set<int> sePlus;
+  set<int> seMinus;
+  rep(i,q){
+    int num,ans;
+    cin >> num >> ans;
+    if(num == 1) {
+      sePlus.insert(ans);
+      seMinus.insert(ans * -1);
+    }
+    else{
+      int minNum = INF32,maxNum = -1;
+      auto itr1 = seMinus.lower_bound(ans * -1);
+      if(itr1 != seMinus.end()) minNum = (*itr1) * -1;
+      auto itr2 = sePlus.lower_bound(ans);
+      if(itr2 != sePlus.end()) maxNum = *itr2;
+      int dif = min(maxNum - ans,ans - minNum);
+      if(dif == INF32) out("-1");
+      else out(ans);
+    }
+  }
+}
