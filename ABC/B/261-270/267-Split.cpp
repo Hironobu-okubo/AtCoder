4a886f1ed755e60ecdc1bfddc7446282df6c66d5
Hironobu-okubo <ee171068oh@gmail.com>
2022-12-02 01:32:01 +0900

    267

--- ABC/B/261-270/267-Split.cpp
+++ ABC/B/261-270/267-Split.cpp
@@ -35,40 +35,63 @@ using pii = pair<int, int>;
 template<typename T> inline bool chmax(T &a, T b) { return ((a < b) ? (a = b, true) : (false)); }
 template<typename T> inline bool chmin(T &a, T b) { return ((a > b) ? (a = b, true) : (false)); }
 
+// int main(){
+//   string s;
+//   cin >> s;
+//   bool isSplit = false;
+//   string ss = "";
+//   (s[6] == '1') ? (ss += "1") : (ss += "0");
+//   (s[3] == '1') ? (ss += "1") : (ss += "0");
+//   (s[1] == '1' || s[7] == '1') ? (ss += "1") : (ss += "0");
+//   (s[4] == '1') ? (ss += "1") : (ss += "0");
+//   (s[2] == '1' || s[8] == '1') ? (ss += "1") : (ss += "0");
+//   (s[5] == '1') ? (ss += "1") : (ss += "0");
+//   (s[9] == '1') ? (ss += "1") : (ss += "0");
+//   string sss = "";
+//   (s[6] == '0') ? (sss += "0") : (sss += "1");
+//   (s[3] == '0') ? (sss += "0") : (sss += "1");
+//   (s[1] == '0' && s[7] == '0') ? (sss += "0") : (sss += "1");
+//   (s[4] == '0') ? (sss += "0") : (sss += "1");
+//   (s[2] == '0' && s[8] == '0') ? (sss += "0") : (sss += "1");
+//   (s[5] == '0') ? (sss += "0") : (sss += "1");
+//   (s[9] == '0') ? (sss += "0") : (sss += "1");
+//   if(s[0] == '0'){
+//     rep(left,6){
+//       reps(right,left + 1,7){
+//         if(ss[left] == '1' && ss[right] == '1' && (right - left >= 2)){
+//           bool isZero = true;
+//           reps(i,left + 1,right){
+//             if(sss[i] == '1') isZero = false;
+//           }
+//           if(isZero) isSplit = true;
+//         }
+//       }
+//     }
+//   }
+//   // out(sss);
+//   // out(ss);
+//   YesNo(isSplit);
+// }
+
+
 int main(){
   string s;
   cin >> s;
   bool isSplit = false;
-  string ss = "";
-  (s[6] == '1') ? (ss += "1") : (ss += "0");
-  (s[3] == '1') ? (ss += "1") : (ss += "0");
-  (s[1] == '1' || s[7] == '1') ? (ss += "1") : (ss += "0");
-  (s[4] == '1') ? (ss += "1") : (ss += "0");
-  (s[2] == '1' || s[8] == '1') ? (ss += "1") : (ss += "0");
-  (s[5] == '1') ? (ss += "1") : (ss += "0");
-  (s[9] == '1') ? (ss += "1") : (ss += "0");
-  string sss = "";
-  (s[6] == '0') ? (sss += "0") : (sss += "1");
-  (s[3] == '0') ? (sss += "0") : (sss += "1");
-  (s[1] == '0' && s[7] == '0') ? (sss += "0") : (sss += "1");
-  (s[4] == '0') ? (sss += "0") : (sss += "1");
-  (s[2] == '0' && s[8] == '0') ? (sss += "0") : (sss += "1");
-  (s[5] == '0') ? (sss += "0") : (sss += "1");
-  (s[9] == '0') ? (sss += "0") : (sss += "1");
-  if(s[0] == '0'){
-    rep(left,6){
-      reps(right,left + 1,7){
-        if(ss[left] == '1' && ss[right] == '1' && (right - left >= 2)){
-          bool isZero = true;
-          reps(i,left + 1,right){
-            if(sss[i] == '1') isZero = false;
-          }
-          if(isZero) isSplit = true;
-        }
-      }
-    }
-  }
-  // out(sss);
-  // out(ss);
+
+  string ss;
+  s[6] == '0' ? ss += '0' : ss += '1';
+  s[3] == '0' ? ss += '0' : ss += '1';
+  (s[1] == '0' && s[7] == '0') ? ss += '0' : ss += '1';
+  s[4] == '0' ? ss += '0' : ss += '1';
+  (s[2] == '0' && s[8] == '0') ? ss += '0' : ss += '1';
+  s[5] == '0' ? ss += '0' : ss += '1';
+  s[9] == '0' ? ss += '0' : ss += '1';
+  if(ss.find("101") != string::npos) isSplit = true;
+  if(ss.find("1001") != string::npos) isSplit = true;
+  if(ss.find("10001") != string::npos) isSplit = true;
+  if(ss.find("100001") != string::npos) isSplit = true;
+  if(ss.find("1000001") != string::npos) isSplit = true;
+  if(s[0] != '0') isSplit = false;
   YesNo(isSplit);
 }
