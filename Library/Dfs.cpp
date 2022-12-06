b419d7662cf9d77c640a40c10be9ec5053fc8151
Hironobu-okubo <ee171068oh@gmail.com>
2022-12-06 17:32:13 +0900

    Dfs

---
+++ Library/Dfs.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+/* alias */
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using vvs = vector<vs>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+using pii = pair<int, int>;
+/* define short */
+#define pb push_back
+#define mp make_pair
+#define all(obj) (obj).begin(), (obj).end()
+#define YESNO(bool) if(bool){cout<<"YES"<<endl;}else{cout<<"NO"<<endl;}
+#define yesno(bool) if(bool){cout<<"yes"<<endl;}else{cout<<"no"<<endl;}
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+/* REP macro */
+#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define rep(i, n) reps(i, 0, n)
+#define repp(i,n) reps(i, 0, n + 1)
+#define rrep(i, n) reps(i, 1, n + 1)
+#define repd(i,n) for(ll i=n-1;i>=0;i--)
+#define rrepd(i,n) for(ll i=n;i>=1;i--)
+#define out(x) cout << x << endl
+//定数
+#define INF32 2147483647 
+#define INF64 9223372036854775807 
+#define MOD 1000000007
+
+template<typename T> inline bool chmax(T &a, T b) { return ((a < b) ? (a = b, true) : (false)); }
+template<typename T> inline bool chmin(T &a, T b) { return ((a > b) ? (a = b, true) : (false)); }
+
+int n;
+vvi p(n);
+vector<bool> visited(n);
+
+void dfs(int pos){
+  rep(i,p[pos].size()){
+    int nxt = p[pos][i];
+    if(visited[nxt] == false){
+      visited[nxt] = true;
+      dfs(nxt);
+    }
+  }
+  return;
+}
+
+int main(){
+  int m;
+  cin >> n >> m;
+  p.resize(n);
+  visited.resize(n,false);
+  rep(i,m){
+    int a,b;
+    cin >> a >> b;
+    a--;b--;
+    p[a].pb(b);
+    p[b].pb(a);
+  }
+  visited[0] = true;
+  dfs(0);
+  rep(i,visited.size()){
+    if(visited[i] == false){
+      out("No");
+      return 0;
+    }
+  }
+  out("Yes");
+}
