190682429be3c6d6e6134888d7551ec8fac5dd05
Hironobu-okubo <ee171068oh@gmail.com>
2022-12-15 06:42:49 +0900

    B062

--- kyopro-tessoku/ch09/B62-PrintaPath.cpp
+++ kyopro-tessoku/ch09/B62-PrintaPath.cpp
@@ -12,6 +12,7 @@ using vvll = vector<vll>;
 using vs = vector<string>;
 using vc = vector<char>;
 using vvc = vector<vc>;
+using vb = vector<bool>;
 using pii = pair<int, int>;
 /* define short */
 #define pb push_back
@@ -36,37 +37,72 @@ using pii = pair<int, int>;
 template<typename T> inline bool chmax(T &a, T b) { return ((a < b) ? (a = b, true) : (false)); }
 template<typename T> inline bool chmin(T &a, T b) { return ((a > b) ? (a = b, true) : (false)); }
 
-vi visited;
-vvi g;
-int n,m;
-vi ans;
-bool isPath = false;
+// vi visited;
+// vvi g;
+// int n,m;
+// vi ans;
+// bool isPath = false;
 
-void dfs(int pos, int n){
-  visited[pos] = 1;
-  rep(i,g[pos].size()){
-    int nxt = g[pos][i];
-    if(visited[nxt] == 0){
-      if(!isPath) ans.pb(nxt);
-      if(nxt == n) isPath = true;
-      dfs(nxt,n); 
+// void dfs(int pos, int n){
+//   visited[pos] = 1;
+//   rep(i,g[pos].size()){
+//     int nxt = g[pos][i];
+//     if(visited[nxt] == 0){
+//       if(!isPath) ans.pb(nxt);
+//       if(nxt == n) isPath = true;
+//       dfs(nxt,n); 
+//     }
+//   }
+//   if(!isPath) ans.pop_back();
+//   return;
+// }
+
+// int main(){
+//   cin >> n >> m;
+//   vi a(m + 1),b(m + 1);
+//   visited.resize(n + 1,0);
+//   g.resize(n + 1);
+//   rrep(i,m){
+//     cin >> a[i] >> b[i];
+//     g[a[i]].pb(b[i]);
+//     g[b[i]].pb(a[i]);
+//   }
+//   ans.pb(1);
+//   dfs(1,n);
+//   rep(i,ans.size()) cout << ans[i] << " ";
+// }
+
+int n;
+vvi s;
+vb visited;
+vi path;
+
+void dfs(int num, vvi &s){
+  visited[num] = true;
+  path.pb(num);
+  rep(i,s[num].size()){
+    int nxt = s[num][i];
+    if(visited[nxt] == false){
+      dfs(nxt,s);
     }
   }
-  if(!isPath) ans.pop_back();
+  if(path[path.size() - 1] != n - 1) path.pop_back();
   return;
 }
 
 int main(){
+  int m;
   cin >> n >> m;
-  vi a(m + 1),b(m + 1);
-  visited.resize(n + 1,0);
-  g.resize(n + 1);
-  rrep(i,m){
-    cin >> a[i] >> b[i];
-    g[a[i]].pb(b[i]);
-    g[b[i]].pb(a[i]);
+  s.resize(n);
+  rep(i,m) {
+    int a,b;
+    cin >> a >> b;
+    a--;b--;
+    s[a].pb(b);
+    s[b].pb(a);
   }
-  ans.pb(1);
-  dfs(1,n);
-  rep(i,ans.size()) cout << ans[i] << " ";
+  visited.resize(n,false);
+  dfs(0,s);
+  rep(i,path.size()) cout << path[i] + 1 << " ";
+  out("");
 }
