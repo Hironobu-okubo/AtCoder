a8e279017ea5d6746f2b79f66115b38a8260f60c
Hironobu-okubo <ee171068oh@gmail.com>
2022-12-16 16:17:42 +0900

    252

---
+++ ABC/C/251-260/252-1Operation.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+/* alias */
+using ull = unsigned long long;
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<long>;
+using vll = vector<long long>;
+using vvi = vector<vi>;
+using vvl = vector<vl>;
+using vvll = vector<vll>;
+using vs = vector<string>;
+using vc = vector<char>;
+using vvc = vector<vc>;
+using vb = vector<bool>;
+using pii = pair<int, int>;
+/* define short */
+#define pb push_back
+#define mp make_pair
+#define all(obj) (obj).begin(), (obj).end()
+#define YESNO(bool) if(bool){cout<<"YES"<<endl;}else{cout<<"NO"<<endl;}
+#define yesno(bool) if(bool){cout<<"yes"<<endl;}else{cout<<"no"<<endl;}
+#define YesNo(bool) if(bool){cout<<"Yes"<<endl;}else{cout<<"No"<<endl;}
+/* REP macro */
+#define reps(i, a, n) for (ll i = (a); i < (ll)(n); ++i)
+#define rep(i, n) reps(i, 0, n)
+#define repp(i,n) reps(i, 0, n + 1)1
+#define rrep(i, n) reps(i, 1, n + 1)
+#define repd(i,n) for(ll i=n-1;i>=0;i--)
+#define rrepd(i,n) for(ll i=n;i>=1;i--)
+#define out(x) cout << x << endl
+//定数
+#define INF32 2147483647 
+#define INF64 9223372036854775807 
+#define MOD 1000000007
+
+template<typename T> inline bool chmax(T &a, T b) { return ((a < b) ? (a = b, true) : (false)); }
+template<typename T> inline bool chmin(T &a, T b) { return ((a > b) ? (a = b, true) : (false)); }
+
+int main(){
+  ll x,a,d,n;
+  cin >> x >> a >> d >> n;
+  ll ans, end, start;
+  if(d >= 0){
+    end = a + d * (n - 1);
+    start = a;
+  }
+  else{
+    end = a;
+    start = a + d * (n - 1);
+    d *= -1;
+  }
+  
+  if(start <= x && x <= end){
+    ll d1;
+    if(d != 0) d1 = (x - start) % d;
+    else d1 = 0;
+    ans = min(d1,d - d1);
+  }
+  else if(x < start) ans = start - x;
+  else if(end < x) ans = x - end;
+  out(ans);
+}
